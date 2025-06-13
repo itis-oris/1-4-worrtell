1a80cb8055fab56f399c2c86ee6db2a39111b19a
Диана Ахмадуллина <153765129+worrtell@users.noreply.github.com>
2025-06-14 02:05:20 +0300

    switching to multi-module mod

--- shop-main/src/main/java/dev/wortel/meshok/entity/UserRole.java
+++
@@ -1,7 +0,0 @@
-package dev.wortel.meshok.entity;
-
-//public enum UserRole {
-//    USER,
-//    ADMIN,
-//    OWNER
-//}
--- shop-main/src/main/java/dev/wortel/meshok/entity/UserStatus.java
+++
@@ -1,7 +0,0 @@
-package dev.wortel.meshok.entity;
-
-//public enum UserStatus {
-//    ACTIVE,
-//    NEED_CONFIRM,
-//    DELETED
-//}
