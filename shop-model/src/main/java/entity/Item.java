4fabedc01d321b6c355a1862556ff3847dfacdc5
Диана Ахмадуллина <153765129+worrtell@users.noreply.github.com>
2025-06-14 01:41:22 +0300

    switching to multi-module mod

---
+++ shop-model/src/main/java/entity/Item.java
@@ -0,0 +1,46 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import static jakarta.persistence.EnumType.ORDINAL;
+
+@Data
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "meshok_id")
+    private Long meshokId;
+    @Column(length = 500)
+    private String name;
+    @Column(length = 500)
+    private String category;
+    private String saleType;
+    private String longevity;
+    private String listDateTime;
+    private String endDateTime;
+    private String quantity;
+    private String sold;
+    private String localDelivery;
+    private String localDeliveryPrice;
+    private String delivery;
+    private String countryDeliveryPrice;
+    private String worldDeliveryPrice;
+    private String condition;
+    private String numberOfPictures;
+    private String picturesFolderPath;
+    private String city;
+    private String status;
+    private String price;
+    @Column(length = 500)
+    private String deliveryText;
+    @Column(length = 2000)
+    private String description;
+    private String format;
+    @Enumerated(ORDINAL)
+    private ItemStatus itemStatus;
+    @Enumerated(ORDINAL)
+    private Source source;
+}
---
+++ shop-model/src/main/java/entity/ItemStatus.java
@@ -0,0 +1,7 @@
+package entity;
+
+public enum ItemStatus {
+    ACTIVE,
+    DELETED,
+    SOLD
+}
