4af2756aa3f44d5569a6e81b58254aea9589e65f
Диана Ахмадуллина <153765129+worrtell@users.noreply.github.com>
2025-06-14 00:19:04 +0300

    cleaning up

--- src/main/java/dev/wortel/meshok/config/CacheConfig.java
+++ src/main/java/dev/wortel/meshok/config/CacheConfig.java
@@ -1,22 +1,22 @@
 package dev.wortel.meshok.config;
 
-//import com.github.benmanes.caffeine.cache.Caffeine;
-//import org.springframework.cache.CacheManager;
-//import org.springframework.cache.annotation.EnableCaching;
-//import org.springframework.cache.caffeine.CaffeineCacheManager;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import java.util.concurrent.TimeUnit;
-//
-//@Configuration
-//@EnableCaching
-//public class CacheConfig {
-//    @Bean
-//    public CacheManager cacheManager() {
-//        CaffeineCacheManager cacheManager = new CaffeineCacheManager("photoCache");
-//        cacheManager.setCaffeine(Caffeine.newBuilder()
-//                .maximumSize(1000)
-//                .expireAfterWrite(24, TimeUnit.HOURS));
-//        return cacheManager;
-//    }
-//}
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("photoCache");
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .maximumSize(1000)
+                .expireAfterWrite(24, TimeUnit.HOURS));
+        return cacheManager;
+    }
+}
